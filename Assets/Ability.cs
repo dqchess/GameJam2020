428ad925d8e3dfc4c23c1b56ff1f34e425e10d83
CodeBishop <scott.bishop.dev@gmail.com>
2020-02-15 15:42:14 -0800

    Rename inventory to Ability

--- Assets/Ability.cs
+++ Assets/Ability.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class inventory : MonoBehaviour
+public class Ability : MonoBehaviour
 {
     public enum DiamondState {
         Unowned, Available, Active
