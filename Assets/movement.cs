cbcb2daab728a0a81712dc5486708f0f091a8d08
dannydeng <dengzecheng@hotmail.com>
2020-02-15 11:46:48 -0800

    movement version 2
    use transform.Translate for movement
    now the cube can rotate

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -4,64 +4,66 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-
-    Rigidbody rb;
-    public float speed = 1f;
-    public float yForce = 200f;
-    public float xForce = 150f;
-    public float zForce = 150f;
-    bool onGround = true;
-    public float jumpTime = 1f;
+    //
+    // Rigidbody rb;
+    // public float speed = 1f;
+    // public float yForce = 200f;
+    // public float xForce = 150f;
+    // public float zForce = 150f;
+    // bool onGround = true;
+    // public float jumpTime = 1f;
+    public float movespeed;
+    public float RotateSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-      rb = GetComponent<Rigidbody>();
+      movespeed = 30f;
+      RotateSpeed = 60f;
+
     }
 
     //Update is called once per frame
     void Update()
     {
-      // if(Input.GetKey("w"))
-      // {
-      //  MoveUp();
-      // }
+     transform.Translate(0f,0f,Input.GetAxis("Vertical") * Time.deltaTime * movespeed);
+     transform.Rotate(0f,Input.GetAxis("Horizontal") * Time.deltaTime * RotateSpeed,0f);
     }
 
-    public void MoveUp()
-    {
-      rb.AddForce(0,0,zForce*Time.deltaTime*speed,ForceMode.VelocityChange);
-    }
-    public void MoveDown()
-    {
-      rb.AddForce(0,0,-zForce*Time.deltaTime*speed,ForceMode.VelocityChange);
-    }
-
-
-    public void MoveLeft()
-    {
-      rb.AddForce(-xForce*Time.deltaTime*speed,0,0,ForceMode.VelocityChange);
-    }
-    public void MoveRight()
-    {
-      rb.AddForce(xForce*Time.deltaTime*speed,0,0,ForceMode.VelocityChange);
-    }
-
-
-    public void jump()
-    {
-      if(onGround)
-      {
-      rb.AddForce(0,yForce*.01f,0,ForceMode.VelocityChange);
-      onGround = false;
-      Invoke("jumpReSet", jumpTime);
-      }
-    }
-
-    void jumpReSet()
-    {
-      onGround = true;
-    }
+    // public void MoveUp()
+    // {
+    //   rb.AddForce(0,0,zForce*Time.deltaTime*speed,ForceMode.VelocityChange);
+    // }
+    // public void MoveDown()
+    // {
+    //   rb.AddForce(0,0,-zForce*Time.deltaTime*speed,ForceMode.VelocityChange);
+    // }
+    //
+    //
+    // public void MoveLeft()
+    // {
+    //   rb.AddForce(-xForce*Time.deltaTime*speed,0,0,ForceMode.VelocityChange);
+    // }
+    // public void MoveRight()
+    // {
+    //   rb.AddForce(xForce*Time.deltaTime*speed,0,0,ForceMode.VelocityChange);
+    // }
+    //
+    //
+    // public void jump()
+    // {
+    //   if(onGround)
+    //   {
+    //   rb.AddForce(0,yForce*.01f,0,ForceMode.VelocityChange);
+    //   onGround = false;
+    //   Invoke("jumpReSet", jumpTime);
+    //   }
+    // }
+    //
+    // void jumpReSet()
+    // {
+    //   onGround = true;
+    // }
 
 
 }
