289a565185522b083607110804c2488cc3b52f2a
Farga Krishnanta <f_krishnanta@live.com>
2020-02-16 02:19:45 -0800

    Add room creation and join with error handler.

--- Assets/NetworkInitiator.cs
+++ Assets/NetworkInitiator.cs
@@ -7,18 +7,19 @@ using Firebase.Unity.Editor;
 using System;
 using Doozy.Engine.UI;
 using UnityEngine.UI;
-
+using UnityEngine.Events;
 
 public class NetworkInitiator : MonoBehaviour
 {
     // Start is called before the first frame update
     public string PlayerID;
-
+    public string RoomID;
     private DatabaseReference dbReference;
+
 
     void Start()
     {
-      
+        StartDB();
     }
 
     void StartDB()
@@ -37,8 +38,6 @@ public class NetworkInitiator : MonoBehaviour
         {
             SetPlayerUniqueID();
         }
-
-        WriteNewMessage(DateTime.Now.Ticks.ToString(), DateTime.Now.Ticks, PlayerID, "HELLO" + DateTime.Now.Ticks);
     }
 
     string SetPlayerUniqueID()
@@ -57,6 +56,57 @@ public class NetworkInitiator : MonoBehaviour
         dbReference.Child("Dummy").Child(messageID).SetRawJsonValueAsync(json);
 
     }
+
+    public async System.Threading.Tasks.Task ConnectToRoomAsync(string roomID, UnityAction<bool, string> callback)
+    {
+        var initialGetResult = await dbReference.Child("Rooms").Child(roomID).GetValueAsync();
+
+        if(initialGetResult.Exists)
+        {
+            DataSnapshot snapshot = initialGetResult;
+
+            if (snapshot.Value == null)
+            {
+                callback(false, "Room with this id doesn't exist.");
+            }
+            else
+            {
+                foreach (var child in snapshot.Children)
+                {
+                    if (child.Key == "PlayerTwoID")
+                    {
+                        if (child.Value.ToString() == "")
+                        {
+                            await dbReference.Child("Rooms").Child(roomID).Child("PlayerTwoID").SetValueAsync(PlayerID);
+                            RoomID = roomID;
+                            callback(true, "Connected!");
+                        }
+                        else
+                        {
+                            callback(false, "Room is full.");
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public async System.Threading.Tasks.Task CreateRoomAsync(string roomID, UnityAction<bool, string> callback)
+    {
+        string json = JsonUtility.ToJson(new Room(roomID, PlayerID, ""));
+
+        var initialGetResult = await dbReference.Child("Rooms").Child(roomID).GetValueAsync();
+        if(initialGetResult.Value == null)
+        {
+            await dbReference.Child("Rooms").Child(roomID).SetRawJsonValueAsync(json);
+            callback(true, "Success! Room is created.");
+        }
+        else
+        {
+            callback(false, "Room with that ID is already created");
+        }
+    }
 }
 
 public class Message
@@ -66,16 +116,25 @@ public class Message
     public long messageDateTime;
     public string message;
 
-    public Message()
-    {
-
-    }
-
     public Message(string messageID, long messageDateTime, string sentBy, string message)
     {
         this.messageID = messageID;
         this.sentBy = sentBy;
         this.messageDateTime = messageDateTime;
         this.message = message;
+    }
+}
+
+public class Room
+{
+    public string RoomID;
+    public string PlayerOneID;
+    public string PlayerTwoID;
+
+    public Room (string RoomID, string PlayerOneID, string PlayerTwoID)
+    {
+        this.RoomID = RoomID;
+        this.PlayerOneID = PlayerOneID;
+        this.PlayerTwoID = PlayerTwoID;
     }
 }
--- Assets/PartnerConnectController.cs
+++ Assets/PartnerConnectController.cs
@@ -8,7 +8,7 @@ using TMPro;
 public class PartnerConnectController : MonoBehaviour
 {
     [SerializeField]
-    private UIButton Connect;
+    private UIButton ConnectButton;
 
     [SerializeField]
     private UIButton CreatePartnerID;
@@ -17,13 +17,48 @@ public class PartnerConnectController : MonoBehaviour
     private TMP_InputField PartnerIDField;
 
     [SerializeField]
+    private TMP_InputField NewPartnerIDField;
+
+    [SerializeField]
     private NetworkInitiator networkInitiator;
 
+    [SerializeField]
+    private TMPro.TMP_Text OnCreateText;
+
+    [SerializeField]
+    private TMPro.TMP_Text OnConnectText;
+
+    public UIView view;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ConnectButton.OnClick.OnTrigger.SetAction((button) => networkInitiator.ConnectToRoomAsync(PartnerIDField.text, OnConnect));
+        CreatePartnerID.OnClick.OnTrigger.SetAction((button) => networkInitiator.CreateRoomAsync(NewPartnerIDField.text, OnCreate));
     }
 
-    
+    public void OnConnect(bool status, string message)
+    {
+        if (!status)
+        {
+            OnConnectText.SetText(message);
+        }
+        else
+        {
+            view.Hide(false);
+        }
+    }
+
+    public void OnCreate(bool status, string message)
+    {
+        if (!status)
+        {
+            OnCreateText.SetText(message);
+        }
+        else
+        {
+            UIView.HideView("Connect Window"); 
+        }
+    }
+
+
 }
