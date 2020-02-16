d8a714ec04e71946d3ae97a68046f375c7b607f4
Nyefari <nyefari@gmail.com>
2020-02-15 23:29:48 -0800

    made antenna glow. made dummy tiles and mats for tiles without textures yet. Finished and tested world generation script. Adjusted player model.

--- Assets/TerrainTiles/LevelGenerator.cs
+++ Assets/TerrainTiles/LevelGenerator.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 using System.Data;
 using System.IO;
 using System.Diagnostics;
-using Microsoft.VisualBasic;
-using Microsoft.VisualBasic.FileIO;
 
 public class LevelGenerator : MonoBehaviour
 {
@@ -20,66 +18,64 @@ public class LevelGenerator : MonoBehaviour
     public GameObject lavaTile;
     public GameObject canyonTile;
     public GameObject blockingTile;
-    public float zstart = -56f;
-    public float xstart = 40f;
+    public float zstart = -224f;
+    public float xstart = -160f;
     float currentX;
     float currentZ;
+    public TextAsset world;
 
     void Start()
     {
         currentX = xstart;
-        using (TextFieldParser parser = new TextFieldParser("world.csv"))
+        string[] tileTable = world.ToString().Split(new[] { System.Environment.NewLine }, System.StringSplitOptions.None);
+        foreach (string tileRow in tileTable) 
         {
-            parser.TextFieldType = FieldType.Delimited;
-            parser.SetDelimiters(",");
-            while (!parser.EndOfData) 
+            currentZ = zstart;
+            string[] tiles = tileRow.Split(',');
+            foreach (string tile in tiles)
             {
-                currentZ = zstart;
-                string[] tiles = parser.ReadFields();
-                foreach (string tile in tiles) 
+                switch (tile)
                 {
-                    switch (tile)
-                    {
-                        case "c":
-                            Instantiate(caveTile, new Vector3(currentX, 0, currentZ), Quaternion.identity);
-                            break;
-                        case "g":
-                            Instantiate(grassTile, new Vector3(currentX, 0, currentZ), Quaternion.identity);
-                            break;
-                        case "e":
-                            Instantiate(dirtTile, new Vector3(currentX, 0, currentZ), Quaternion.identity);
-                            break;
-                        case "f":
-                            Instantiate(forestTile, new Vector3(currentX, 0, currentZ), Quaternion.identity);
-                            break;
-                        case "i":
-                            Instantiate(iceTile, new Vector3(currentX, 0, currentZ), Quaternion.identity);
-                            break;
-                        case "l":
-                            Instantiate(lavaTile, new Vector3(currentX, 0, currentZ), Quaternion.identity);
-                            break;
-                        case "t":
-                            Instantiate(stormTile, new Vector3(currentX, 0, currentZ), Quaternion.identity);
-                            break;
-                        case "u":
-                            Instantiate(blockingTile, new Vector3(currentX, 0, currentZ), Quaternion.identity);
-                            break;
-                        case "v":
-                            Instantiate(canyonTile, new Vector3(currentX, 0, currentZ), Quaternion.identity);
-                            break;
-                        case "r":
-                            Instantiate(waterTile, new Vector3(currentX, 0, currentZ), Quaternion.identity);
-                            break;
-                        default:
-                            Instantiate(dirtTile, new Vector3(currentX, 0, currentZ), Quaternion.identity);
-                            break;
-                    }
-                    currentZ += 4f;
+                    case "c":
+                        Instantiate(caveTile, new Vector3(currentX, 0, currentZ), Quaternion.identity);
+                        break;
+                    case "g":
+                        Instantiate(grassTile, new Vector3(currentX, 0, currentZ), Quaternion.identity);
+                        break;
+                    case "e":
+                        Instantiate(dirtTile, new Vector3(currentX, 0, currentZ), Quaternion.identity);
+                        break;
+                    case "f":
+                        Instantiate(forestTile, new Vector3(currentX, 0, currentZ), Quaternion.identity);
+                        break;
+                    case "i":
+                        Instantiate(iceTile, new Vector3(currentX, 0, currentZ), Quaternion.identity);
+                        break;
+                    case "l":
+                        Instantiate(lavaTile, new Vector3(currentX, 0, currentZ), Quaternion.identity);
+                        break;
+                    case "t":
+                        Instantiate(stormTile, new Vector3(currentX, 0, currentZ), Quaternion.identity);
+                        break;
+                    case "u":
+                        Instantiate(blockingTile, new Vector3(currentX, 0, currentZ), Quaternion.identity);
+                        break;
+                    case "v":
+                        Instantiate(canyonTile, new Vector3(currentX, 0, currentZ), Quaternion.identity);
+                        break;
+                    case "r":
+                        Instantiate(waterTile, new Vector3(currentX, 0, currentZ), Quaternion.identity);
+                        break;
+                    default:
+                        Instantiate(dirtTile, new Vector3(currentX, 0, currentZ), Quaternion.identity);
+                        break;
                 }
-
-                currentX += 4f;
+                currentZ += 4f;
             }
-        }
+
+            currentX += 4f;
+        }    
+        
     }
 
     // Update is called once per frame
