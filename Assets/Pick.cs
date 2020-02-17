312a398297fa59226dd37a0b9f571090e23a7861
Nyefari <nyefari@gmail.com>
2020-02-16 16:21:49 -0800

    Ability and animation work.

--- Assets/Pick.cs
+++ Assets/Pick.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 public class Pick : MonoBehaviour
 {
     bool carrying;
-    public Transform theDest;
+    private Transform theDest;
+    public NetworkMessenger netwrk;
+    public Animator animator;
+    public AudioSource actionAudio;
+    public AudioClip pickupSound;
+    public AudioClip chopSound;
+    public AudioClip breakSound;
+    public string newAbility;
 
     bool ableToAcquireAbility;
     bool ableToGrabandHold;
@@ -36,27 +43,43 @@ public class Pick : MonoBehaviour
 
         if(ableToAcquireAbility && other.tag == "ability")
         {
+            actionAudio.PlayOneShot(pickupSound);
+            Attack();
+            newAbility = other.gameObject.name;
+            netwrk.SendAbilityMessage("new",newAbility);
             Destroy(other.gameObject);
+
             // Message Packer
         }
 
         if(ableToGrabandHold && other.tag == "item")
         {
+            actionAudio.PlayOneShot(pickupSound);
+            Attack();
             Destroy(other.gameObject);
             // Update GUI
         }
 
         if (ableToBreakRocks && other.tag == "rock")
         {
+            actionAudio.PlayOneShot(breakSound);
+            Attack();
             Destroy(other.gameObject);
         }
 
         if(ableToChopTree && other.tag == "tree")
         {
+            actionAudio.PlayOneShot(chopSound);
+            Attack();
             Destroy(other.gameObject);
         }
 
         
+    }
+
+    void Attack()
+    {
+        animator.SetTrigger("Attack");
     }
 
 }
